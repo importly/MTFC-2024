07cda339fc673a719e4daa196d5142111835e31b
Ubuntu <prithish@GeoBlendServer.gy45cpx3mu5erjohjikgoh0rdg.bx.internal.cloudapp.net>
2024-03-03 01:01:14 +0000

    latest

--- scraper.py
+++ scraper.py
@@ -14,78 +14,118 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_service = ChromeService(executable_path='/home/prithish/MTFC/MTFC-2024/chromedriver')
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
 def get_text(url):
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    #https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.94/win64/chromedriver-win64.zip go here for windows version
-    chrome_service = ChromeService(executable_path='/Users/m296082/Downloads/chromedriver-mac-arm64/chromedriver')
-    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
-    driver.get(url)
+    try:
+        driver.get(url)
 
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.TAG_NAME, 'body'))
-    )
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.TAG_NAME, 'body'))
+        )
 
-    full_text = driver.find_element(By.TAG_NAME, 'body').text
-    driver.quit()
+        full_text = driver.find_element(By.TAG_NAME, 'body').text
+    except:
+        full_text = ""
     return full_text
 
 api_key = os.getenv("API_KEY")
 console_id = os.getenv("CONSOLE_ID")
 
-queries = ["Is Climate Change Real?"]
+queries = [ 
+    #"How should the United States treat Russia?",
+    #"How effective was the War on Drugs?",
+    #"How should border security be enforced?",
+    #"Support for healthcare reform.",
+    "Is climate change important?",
+    "Should the United States keep funding NASA?",
+    "Should there be more free trade agreements?",
+    "US foreign policy in the Middle East",
+    "racial equality government policies",
+    "gender equality governemnt policies",
+    "Is nuclear power safe?",
+    "What are the views on Hydroelectric Power?",
+    "What are the views on social media, interconnectivity, and globalization?"
+]
 
-incr = 58
+incr = 3
 max_year = 2024
-min_year = 2000
-upper_month = 0
+min_year = 1974
+init_month = 2
 month_incr = incr % 12
 year_incr = incr // 12
-upper_year = max_year
-lower_year = upper_year - year_incr
 
+#print("hello")
 for query in queries:
+    upper_month = init_month
+    upper_year = max_year
+    lower_year = upper_year - year_incr
+    noData = 0
+    directory = os.fsencode("/home/prithish/MTFC/MTFC-2024")
+    files = os.listdir(directory)
+    #if any(query in file.replace(" ", "_") for file in files)
+    #    continue
+    for file in files:
+        filename = os.fsdecode(file)
+        print(filename)
+        if filename.endswith(".py") or filename.endswith(".csv") or filename == ".git" or filename == ".env" or filename == "chromedriver": 
+            continue
+        else:
+            print(f"/home/prithish/MTFC/MTFC-2024/{filename}")
+            os.remove(f"/home/prithish/MTFC/MTFC-2024/{filename}")
+    
     dataset = {
         "title": [],
         "link": [],
-        "determined_date": [],
+        #"determined_date": [],
         "date_range": [],
         "content": [],
         "snippet": []
     }
-    while (upper_year > min_year):
+    while (upper_year >= min_year):
+        if noData > 10:
+            break
         lower_month = upper_month - month_incr
         if lower_month < 0:
             lower_month += 12
             lower_year -= 1
         print(f"https://www.googleapis.com/customsearch/v1?key={api_key}&cx={console_id}&q={query}&start={1}&num=10&sort=date:r:{lower_year}{'{:02d}'.format(lower_month)}00:{upper_year}{'{:02d}'.format(upper_month)}00")
-        res = requests.get(f"https://www.googleapis.com/customsearch/v1?key={api_key}&cx={console_id}&q={query}&start={1}&num=10&sort=date:r:{lower_year}{'{:02d}'.format(lower_month)}00:{upper_year}{'{:02d}'.format(upper_month)}00").json()    
+        res = requests.get(f"https://www.googleapis.com/customsearch/v1?key={api_key}&cx={console_id}&q={query}&start={1}&num=10&sort=date:r:{lower_year}{'{:02d}'.format(lower_month)}00:{upper_year}{'{:02d}'.format(upper_month)}00").json()
         upper_year = lower_year
         lower_year -= year_incr
         upper_month = lower_month
         if "items" in res:
+            noData = 0
+            print(len(res["items"]))
             for i, result in enumerate(res["items"]):
                 print(result["link"])
                 content = get_text(result["link"])
-                date = "NA"
-                try:
-                    res = requests.head(result["link"])
-                    date_data = res.headers.get("Last-Modified")
-                    if date_data:
-                        date = date_data
-                    else:
-                        meta = result["pagemap"]["metatags"][0]
-                        res = [val for key, val in meta.items() if "date" in key]
-                        if res:
-                            date = res[0]
-                except:
-                    pass
+                if content == "":
+                    continue
+                #date = "NA"
+                #try:
+                #    res = requests.head(result["link"])
+                #    date_data = res.headers.get("Last-Modified")
+                #    if date_data:
+                #        date = date_data
+                #    else:
+                #        meta = result["pagemap"]["metatags"][0]
+                #        res = [val for key, val in meta.items() if "date" in key]
+                #        if res:
+                #            date = res[0]
+                #except:
+                    #pass
                 dataset["title"].append(result["title"])
                 dataset["link"].append(result["link"])
-                dataset["determined_date"].append(date)
+                #dataset["determined_date"].append(date)
                 dataset["date_range"].append(f"{lower_year}{lower_month}-{upper_year}{upper_month}")
                 dataset["content"].append(content)
                 dataset["snippet"].append(result["snippet"])
+        else:
+            noData += 1
 
-            datasetFile = pd.DataFrame(dataset)
-            datasetFile.to_csv(f"{query}.csv")                                  
+        datasetFile = pd.DataFrame(dataset)
+        datasetFile.to_csv(f"{query.replace(' ', '_')}{min_year}-{max_year}by{incr}months.csv")                                  
